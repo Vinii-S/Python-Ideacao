d797400127b0026d03c4f06f621cdb2c2889e6dd
Vinícius Santos <viny070805@gmail.com>
2025-05-28 10:06:12 -0300

    Novas Questões

---
+++ Python/Lista de Repeticao/13. Potencia.py
@@ -0,0 +1,6 @@
+base = int(input("Digite um numero para ser a base: "))
+expoente = int(input("Digite o expoente da base anterior: "))
+
+potencia = base ** expoente
+
+print(f"{base} elevado a {expoente} é = {potencia}")
