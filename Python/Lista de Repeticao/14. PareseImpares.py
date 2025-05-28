5e84032754f6ac98d9d3f2cd4a2fb174f52dede7
Vinícius Santos <viny070805@gmail.com>
2025-05-28 10:37:34 -0300

    Novas Questões

--- Python/Lista de Repeticao/14. PareseImpares.py
+++ Python/Lista de Repeticao/14. PareseImpares.py
@@ -1,2 +1,11 @@
+listaPares = []
+listaImpares = []
 for num in range(10):
+    numero = int(input("Digite um número: "))
+    if(numero%2==0):
+        listaPares.append(numero)
+    else:
+        listaImpares.append(numero)
     
+print(f"Dentre os 10 números que você digitou {len(listaPares)} deles são pares, e eles são: {listaPares}")
+print(f"Dentre os 10 números que você digitou {len(listaImpares)} deles são ímpares e eles são: {listaImpares}")
---
+++ Python/Lista de Repeticao/16. Fibbo500.py
@@ -0,0 +1,10 @@
+a=0
+b=1
+c=0
+print(a)
+print(b)
+while c<500:
+    c = a + b
+    print(c)
+    a = b
+    b = c
